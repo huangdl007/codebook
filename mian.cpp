8a8c8d5d980e753bf1f0a09da90c1564bbe53be1
lym1992 <254565361@qq.com>
2014-11-12 15:54:43 +0800

    add a video file

---
+++ mian.cpp
@@ -0,0 +1,28 @@
+#include <cv.h>           
+#include <highgui.h>
+#include <cxcore.h>
+
+int main()
+{
+	CvCapture* capture;
+	IplImage* frame;
+	capture = cvCreateFileCapture("codebook/tree.avi");
+
+	if(!capture)
+	{
+		std::cout << "Could not open capture" << std::endl;
+	}
+
+	cvNamedWindow("Raw Frame");
+	int count = 0;
+	while(frame = cvQueryFrame(capture))
+	{
+		cvShowImage("Raw Frame", frame);
+		count++;
+	}
+
+	std::cout << count << std::endl;
+
+	cvWaitKey(0);
+	return 0;
+}
