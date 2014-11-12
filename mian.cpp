227183feb9d8910c93217f9aa43c69050befa1f7
lym1992 <254565361@qq.com>
2014-11-12 17:37:01 +0800

    ..

--- mian.cpp
+++ mian.cpp
@@ -2,10 +2,41 @@
 #include <highgui.h>
 #include <cxcore.h>
 
+using namespace cv;
+
+#define CHANNELS 3
+
+typedef struct CodeWord
+{
+	uchar IHigh[CHANNELS];
+	uchar ILow[CHANNELS];
+	uchar max[CHANNELS];
+	uchar min[CHANNELS];
+	int t_last;
+	int stale;
+}CW;
+
+typedef struct CodeBook
+{
+	CW **cw;
+	int numEntries;
+	int t;
+}CB;
+
 int main()
 {
+	//declare variables
 	CvCapture* capture;
-	IplImage* frame;
+	IplImage* rawFrame;
+	CB* cB;
+
+	//initialize variables
+
+	//process every frame of the video
+
+	//release resources
+
+	
 	capture = cvCreateFileCapture("codebook/tree.avi");
 
 	if(!capture)
@@ -14,15 +45,10 @@ int main()
 	}
 
 	cvNamedWindow("Raw Frame");
-	int count = 0;
-	while(frame = cvQueryFrame(capture))
-	{
-		cvShowImage("Raw Frame", frame);
-		count++;
-	}
+	
 
-	std::cout << count << std::endl;
+	unsigned P1 = 0xff;
+	std::cout << P1;
 
-	cvWaitKey(0);
 	return 0;
 }
